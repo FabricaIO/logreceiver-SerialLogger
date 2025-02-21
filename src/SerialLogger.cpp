0c8f0a0a5e60310fe9513790ca08eab5c30a79ed
Sam Groveman <samgroveman@outlook.com>
2025-02-20 20:57:25 -0500

    Removed USBCDC support

--- src/SerialLogger.cpp
+++ src/SerialLogger.cpp
@@ -8,36 +8,12 @@ SerialLogger::SerialLogger(HardwareSerial* HWSerial, int Baud) {
 	baud = Baud;
 }
 
-/// @brief Creates a new serial logger
-/// @param HWSerial Pointer to a hardware serial object to use
-/// @param Baud The baud rate to use
-SerialLogger::SerialLogger(USBCDC* HWSerial, int Baud) {
-	usbcdc = HWSerial;
-	baud = Baud;
-	use_usb = true;
-}
-
-/// @brief Creates a new serial logger
-/// @param HWSerial Pointer to a hardware serial object to use
-/// @param Baud The baud rate to use
-SerialLogger::SerialLogger(HWCDC* HWSerial, int Baud) {
-	hwcdc = HWSerial;
-	baud = Baud;
-	use_hwcdc = true;
-}
-
 /// @brief Starts the serial logger
 /// @return True on success
 bool SerialLogger::begin() {
 	Description.name = "Serial Logger";
 	Description.version = "0.8";
-	if (use_usb) {
-		usbcdc->begin(baud);
-	} else if (use_hwcdc) {
-		hwcdc->begin(baud);
-	} else {
-		serial->begin(baud);
-	}
+	serial->begin(baud);
 	return true;
 }
 
@@ -45,13 +21,7 @@ bool SerialLogger::begin() {
 /// @param message The char to write
 /// @return True on success
 bool SerialLogger::receiveMessage(char message) {
-	if (use_usb) {
-		usbcdc->print(message);
-	} else if (use_hwcdc) {
-		hwcdc->print(message);
-	} else {
-		serial->print(message);
-	}
+	serial->print(message);
 	return true;
 }
 
@@ -59,12 +29,6 @@ bool SerialLogger::receiveMessage(char message) {
 /// @param message The string to write
 /// @return True on success
 bool SerialLogger::receiveMessage(String message) {
-	if (use_usb) {
-		usbcdc->print(message);
-	} else if (use_hwcdc) {
-		hwcdc->print(message);
-	} else {
-		serial->print(message);
-	}
+	serial->print(message);
 	return true;
 }
--- src/SerialLogger.h
+++ src/SerialLogger.h
@@ -10,26 +10,12 @@
 class SerialLogger : public LogReceiver {
 	public:
 		SerialLogger(HardwareSerial* HWSerial, int Baud = 115200);
-		SerialLogger(USBCDC* HWSerial, int Baud = 115200);
-		SerialLogger(HWCDC* HWSerial, int Baud = 115200);
 		bool begin();
 		bool receiveMessage(char message);
 		bool receiveMessage(String message);
 	protected:
 		/// @brief Pointer to hardware serial to use
 		HardwareSerial* serial;
-
-		/// @brief Pointer to the USBCDC serial device to use
-		USBCDC* usbcdc;
-
-		/// @brief Pointer to the HWCDC serial device to use
-		HWCDC* hwcdc;
-		
-		/// @brief True if using USBCDC
-		bool use_usb = false;
-
-		/// @brief True if using HWCDC
-		bool use_hwcdc = false;
 
 		/// @brief The baud rate to use
 		int baud;
